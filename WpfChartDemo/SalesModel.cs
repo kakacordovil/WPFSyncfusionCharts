e72867a2ed70bd31a00e30c064074c95b9925a3e
Karllana Cordovil <karllanacordovil@oryon.solutions>
2023-03-04 15:58:41 +0000

    added wpfchartdemo project

---
+++ WpfChartDemo/SalesModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfChartDemo
+{
+    class SalesModel
+    {
+        public string Year { get; set; } =  "";
+        public double ProductA { get; set; }
+        public double ProductB { get; set; }
+    }
+}
---
+++ WpfChartDemo/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfChartDemo
+{
+    class ViewModel
+    {
+        public List<SalesModel> Data { get; set; }
+        public ViewModel() 
+        { 
+            Data = new List<SalesModel>()
+            { 
+                new SalesModel { Year= "2014", ProductA=300, ProductB=200 },
+                new SalesModel { Year= "2015", ProductA=450, ProductB=500 },
+                new SalesModel { Year= "2016", ProductA=400, ProductB=300 },
+                new SalesModel { Year= "2017", ProductA=550, ProductB=500 },
+                new SalesModel { Year= "2018", ProductA=650, ProductB=450 }
+            };
+        }
+    }
+}
